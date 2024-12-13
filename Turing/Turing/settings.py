ad61879f3e63678e8f75359cfeccdeff46690531
leonardo2322 <joys23services@gmail.com>
2024-12-13 16:55:46 -0400

    error de subida de datos

--- Turing/Turing/settings.py
+++ Turing/Turing/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-g3(2e)g6aw#xe1!+%7lq)bxj18xx!s7@ckk(k%xh1s=u5h%cc('
+SECRET_KEY = config('SECRETKEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
